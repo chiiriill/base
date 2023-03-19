3ed0aada1daa017a3f4b4c5a0284ede6524a2634
chiiriill <chiiriill@yandex.ru>
2023-03-19 16:40:25 +0300

    Tips&Tricks v1.4

--- test.py
+++ test.py
@@ -24,9 +24,3 @@ suit_values = dict(spades=3, hearts=2, diamonds=1, clubs=0)
 def spades_high(card):
     rank_value = FrenchDeck.ranks.index(card.rank)
     return rank_value * len(suit_values) + suit_values[card.suit]
-
-if __name__=="__main__":
-    deck = FrenchDeck()
-    
-    for card in sorted(deck, key=spades_high): # doctest: +ELLIPSIS
-        print(card)
